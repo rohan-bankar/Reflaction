2ce5a55648176aa471af0df50ea001f48ce7f7c2
rohan-bankar <rohanbankar011@gmail.com>
2024-12-09 13:48:33 +0530

    LICENSE

--- client/react-app/src/components/Footer/Footer.jsx
+++ client/react-app/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
       <footer className='py-5 px-6 bg-orange-300'>
         Copyright © 2024 : 
-                        <a href="" className=" underline text-sky-800 hover:text-sky-500 ml-1">
+                        <a href="https://github.com/rohan-bankar/Reflaction/blob/main/LICENSE" className=" underline text-sky-800 hover:text-sky-500 ml-1">
                         Reflectify
                         </a>
       </footer>
